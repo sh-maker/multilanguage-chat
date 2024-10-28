6c95e4abf19aacc87bc64328f428926c0704d381
Syed Shoaib Noor <syedshoaibnoor44@gmail.com>
2024-10-28 21:04:52 +0530

    allowed host to all

--- multilingual_chat/settings.py
+++ multilingual_chat/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
